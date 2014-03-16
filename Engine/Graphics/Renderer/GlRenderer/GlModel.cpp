c875e7451efbda354b6de8b38acee1bd93d98ca3
Narendra Umate <narendraumate@gmail.com>
2014-03-16 16:56:12 -0700

    Fix.

--- Engine/Graphics/Renderer/GlRenderer/GlModel.cpp
+++ Engine/Graphics/Renderer/GlRenderer/GlModel.cpp
@@ -24,19 +24,19 @@ namespace Engine
 			
 			struct VertexData
 			{
-				GLubyte color[4];
 				GLfloat position[2];
+				GLubyte color[4];
 			};
 			
 			VertexData vertices[6] =
 			{
-				{{ 255,   0,   0, 255 }, { -0.90, -0.90 }},  // Triangle 1
-				{{   0, 255,   0, 255 }, {  0.85, -0.90 }},
-				{{   0,   0, 255, 255 }, { -0.90,  0.85 }},
+				{{ -0.90, -0.90 }, { 255,   0,   0, 255 }},  // Triangle 1
+				{{  0.85, -0.90 }, {   0, 255,   0, 255 }},
+				{{ -0.90,  0.85 }, {   0,   0, 255, 255 }},
 				
-				{{   0, 255, 255, 255 }, {  0.90, -0.85 }},  // Triangle 2
-				{{ 255,   0, 255, 255 }, {  0.90,  0.90 }},
-				{{ 255, 255,   0, 255 }, { -0.85,  0.90 }}
+				{{  0.90, -0.85 }, {   0, 255, 255, 255 }},  // Triangle 2
+				{{  0.90,  0.90 }, { 255,   0, 255, 255 }},
+				{{ -0.85,  0.90 }, { 255, 255,   0, 255 }}
 			};
 			
 			glGenVertexArrays(NumVAOs, VAOs);
@@ -47,8 +47,8 @@ namespace Engine
 			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 			
 			glUseProgram(ProgramID);
-			glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), BUFFER_OFFSET(sizeof(vertices[0].color)));
-			glVertexAttribPointer(vColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(VertexData), BUFFER_OFFSET(0));
+			glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), BUFFER_OFFSET(0));
+			glVertexAttribPointer(vColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(VertexData), BUFFER_OFFSET(sizeof(vertices[0].position)));
 			
 			glEnableVertexAttribArray(vPosition);
 			glEnableVertexAttribArray(vColor);
