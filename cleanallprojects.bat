3ccd2d7b4c5345515ee199679e2b1276ceffb5b4
Narendra Umate <narendraumate@gmail.com>
2014-04-10 14:02:43 -0400

    premake.

--- cleanallprojects.bat
+++ cleanallprojects.bat
@@ -1,1 +1,1 @@
-Tools\Windows\premake4 --file=premake4.lua --os=windows clean
+Tools\Windows\premake4\premake4 --file=premake4.lua --os=windows clean
--- createallprojects.bat
+++ createallprojects.bat
@@ -1,1 +1,1 @@
-Tools\Windows\premake4 --file=premake4.lua --os=windows --platform=x64 vs2010
+Tools\Windows\premake4\premake4 --file=premake4.lua --os=windows --platform=x64 vs2012
