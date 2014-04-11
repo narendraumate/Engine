adf01998798ac8ba5017bc23518d2b55568bb55d
Narendra Umate <narendraumate@gmail.com>
2014-04-10 20:44:52 -0400

    premake.

--- createallprojects.sh
+++ createallprojects.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 case "$OSTYPE" in
-	darwin*)	./Tools/MacOS/premake4 --file=premake4.lua --os=macosx --platform=universal xcode4;;
-	linux*)		./Tools/Linux/premake4 --file=premake4.lua --os=linux gmake;;
+	darwin*) ./Tools/MacOS/premake4/premake4 --file=premake4.lua --os=macosx --platform=universal xcode4;;
+	linux*) ./Tools/Linux/premake4/premake4 --file=premake4.lua --os=linux gmake;;
 esac
