e5298716dd0727daf85ab622c51bbb03b06ff5cd
Narendra Umate <narendraumate@gmail.com>
2014-05-11 03:53:20 -0400

    Fix. Linux.

--- Application/Linux/LinuxMain.cpp
+++ Application/Linux/LinuxMain.cpp
@@ -5,57 +5,57 @@
 
 Window createWindow( Display *dpy, GLXContext *ctx, unsigned int width, unsigned int height )
 {
-   int attrib[] = { GLX_RGBA,
-		    GLX_RED_SIZE, 1,
-		    GLX_GREEN_SIZE, 1,
-		    GLX_BLUE_SIZE, 1,
-		    GLX_DOUBLEBUFFER,
-		    None };
-   int scrnum;
-   XSetWindowAttributes attr;
-   unsigned long mask;
-   Window root;
-   Window win;
-   XVisualInfo *visinfo;
+	int attrib[] = { GLX_RGBA,
+		GLX_RED_SIZE, 1,
+		GLX_GREEN_SIZE, 1,
+		GLX_BLUE_SIZE, 1,
+		GLX_DOUBLEBUFFER,
+		None };
+	int scrnum;
+	XSetWindowAttributes attr;
+	unsigned long mask;
+	Window root;
+	Window win;
+	XVisualInfo *visinfo;
 
-   //printf("DefaultScreen\n");
-   scrnum = DefaultScreen( dpy );
-   //printf("RootWindow\n");
-   root = RootWindow( dpy, scrnum );
+	//printf("DefaultScreen\n");
+	scrnum = DefaultScreen( dpy );
+	//printf("RootWindow\n");
+	root = RootWindow( dpy, scrnum );
 
-   //printf("glXChooseVisual\n");
-   visinfo = glXChooseVisual( dpy, scrnum, attrib );
-   if (!visinfo) {
-      //printf("Error: couldn't get an RGB, Double-buffered visual\n");
-      //exit(1);
-	   return win;
-   }
+	//printf("glXChooseVisual\n");
+	visinfo = glXChooseVisual( dpy, scrnum, attrib );
+	if (!visinfo) {
+		//printf("Error: couldn't get an RGB, Double-buffered visual\n");
+		//exit(1);
+		return win;
+	}
 
-   /* window attributes */
-   attr.background_pixel = 0;
-   attr.border_pixel = 0;
-   //printf("XCreateColormap\n");
-   attr.colormap = XCreateColormap( dpy, root, visinfo->visual, AllocNone);
-   attr.event_mask = StructureNotifyMask | ExposureMask;
-   mask = CWBackPixel | CWBorderPixel | CWColormap | CWEventMask;
+	/* window attributes */
+	attr.background_pixel = 0;
+	attr.border_pixel = 0;
+	//printf("XCreateColormap\n");
+	attr.colormap = XCreateColormap( dpy, root, visinfo->visual, AllocNone);
+	attr.event_mask = StructureNotifyMask | ExposureMask;
+	mask = CWBackPixel | CWBorderPixel | CWColormap | CWEventMask;
 
-   //printf("XCreateWindow\n");
-   win = XCreateWindow( dpy, root, 0, 0, width, height,
-		        0, visinfo->depth, InputOutput,
-		        visinfo->visual, mask, &attr );
+	//printf("XCreateWindow\n");
+	win = XCreateWindow( dpy, root, 0, 0, width, height,
+						0, visinfo->depth, InputOutput,
+						visinfo->visual, mask, &attr );
 
-   //printf("glXCreateContext\n");
-   *ctx = glXCreateContext( dpy, visinfo, NULL, True );
-   if (!(*ctx)) {
-      //printf("Error: glXCreateContext failed\n");
-      //exit(1);
-	   return win;
-   }
+	//printf("glXCreateContext\n");
+	*ctx = glXCreateContext( dpy, visinfo, NULL, True );
+	if (!(*ctx)) {
+		//printf("Error: glXCreateContext failed\n");
+		//exit(1);
+		return win;
+	}
 
-   //printf("glXMakeCurrent\n");
-   glXMakeCurrent( dpy, win, *ctx );
+	//printf("glXMakeCurrent\n");
+	glXMakeCurrent( dpy, win, *ctx );
 
-   return win;
+	return win;
 }
 
 int main( int argc, char *argv[] )
@@ -68,24 +68,24 @@ int main( int argc, char *argv[] )
 	getSettings()->setHeight(applicationHeight);
 	getSettings()->setTitle(applicationName);
 
-   Display *dpy;
-   GLXContext ctx;
-   Window win;
+	Display *dpy;
+	GLXContext ctx;
+	Window win;
 
-   //printf("XOpenDisplay\n");
-   dpy = XOpenDisplay(NULL);
+	//printf("XOpenDisplay\n");
+	dpy = XOpenDisplay(NULL);
 
-   win = createWindow( dpy, &ctx, applicationWidth, applicationHeight );
+	win = createWindow( dpy, &ctx, applicationWidth, applicationHeight );
 
-   initializeMain();
+	initializeMain();
 
-   getRenderer()->setContextObj(ctx);
+	getRenderer()->setContextObj(ctx);
 
-   XMapWindow( dpy, win );
+	XMapWindow( dpy, win );
 
 	while(runMain());
 
 	deinitializeMain();
 
-   return 0;
+	return 0;
 }
